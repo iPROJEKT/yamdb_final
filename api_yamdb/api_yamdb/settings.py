f5c03603cf7148b53a6935d4439b049ed064ea6b
iPROJEKT <108357574+iPROJEKT@users.noreply.github.com>
2023-02-18 16:20:41 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
-    'SECRET_KEYY',
+    'SECRET_KEY',
     default='string'
 )
 # SECURITY WARNING: don't run with debug turned on in production!
